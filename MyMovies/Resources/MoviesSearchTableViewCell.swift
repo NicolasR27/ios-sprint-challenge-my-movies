f8c7b5110f6a8e6dbd28fd0cc4fa751cdd92fe40
Nicolas Rios <nickrios33@yahoo.com>
2020-02-24 00:54:12 -0800

    finished challenge

---
+++ MyMovies/Resources/MoviesSearchTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  MoviesSearchTableViewCell.swift
+//  MyMovies
+//
+//  Created by Nicolas Rios on 2/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieSearchTableViewCell: UITableViewCell {
+    var movie: MovieRepresentation? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    var delegate: MovieSearchTableViewCellDelegate?
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    
+    private func updateViews() {
+        guard let movie = movie else { return }
+        titleLabel.text = movie.title
+        addButton.setTitle("Add Movie", for: .normal)
+    }
+    
+    @IBAction func addMovie(_ sender: Any) {
+        delegate?.addNewMovie(cell: self)
+        addButton.setTitle("Added", for: .normal)
+    }
+    
+}
+
+protocol MovieSearchTableViewCellDelegate: class {
+    func addNewMovie(cell: MovieSearchTableViewCell)
+}
