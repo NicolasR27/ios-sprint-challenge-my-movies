36777c61ccaeb88f3493d8099c9aa4c241a07d98
Nicolas Rios <nickrios33@yahoo.com>
2020-10-31 19:03:26 -0700

    Update MyMoviesTableViewController.swift

--- MyMovies/View Controllers/MyMoviesTableViewController.swift
+++ MyMovies/View Controllers/MyMoviesTableViewController.swift
@@ -1,10 +1,4 @@
-//
-//  MyMoviesTableViewController.swift
-//  MyMovies
-//
-//  Created by Spencer Curtis on 8/17/18.
-//  Copyright © 2018 Lambda School. All rights reserved.
-//
+
 
 import UIKit
 import CoreData
